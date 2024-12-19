aa973cf426326faaced2ce1a245b2b441ea23ecd
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-19 13:30:18 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum upgrade -y
-sudo yum install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-sudo yum install ruby -y
-sudo yum install wget -y
-cd /home/ec2-user
-wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
+sudo yum install httpd -y
+sudo yum install git -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl status httpd
