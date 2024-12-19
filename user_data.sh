2a5edf6a05f30bc4fc3bf2b9fda8e356d9bb7a3f
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-19 15:14:41 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
-sudo yum update
+sudo yum update -y
 sudo yum upgrade -y
+sudo amazon-linux-extras enable nginx1
 sudo yum install nginx -y
-sudo yum update
-sudo yum install ruby-full -y
-sudo yum install wget
+sudo yum install ruby -y
+sudo yum install wget -y
+sudo systemctl start nginx
+sudo systemctl enable nginx
 cd /home/ec2-user
 wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
 chmod +x ./install
