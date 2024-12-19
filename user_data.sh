f549771329f240e90b38806f5e206ff40e315f22
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-19 12:56:04 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -2,10 +2,5 @@
 sudo yum update
 sudo yum upgrade -y
 sudo yum install nginx -y
-sudo yum update
-sudo yum install ruby-full -y
-sudo yum install wget
-cd /home/ec2-user
-wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
+sudo systemctl start nginx
+sudo systemctl enable nginx
