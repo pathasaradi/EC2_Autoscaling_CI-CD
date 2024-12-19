3e0022ca9d5860d672e659dd63214e0efc217fe9
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-19 22:41:01 +0530

    Delete validate_service.sh

--- validate_service.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Validate that NGINX is running
-if systemctl is-active --quiet nginx; then
-    echo "NGINX is running."
-    exit 0
-else
-    echo "NGINX is not running."
-    exit 1
-fi
