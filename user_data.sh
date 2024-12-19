5dc2e7b2cfce550f6b45445922225b7f55bcc527
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-19 17:21:16 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -11,4 +11,5 @@ cd /home/ec2-user
 wget https://aws-codedeploy-us-west-2.s3.us-west-2.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
+sudo yum install -y aws-codedeploy-agent
 sudo service codedeploy-agent start
