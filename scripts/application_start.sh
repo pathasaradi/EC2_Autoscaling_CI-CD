3d793d52870874e33811b4772454aff242edfbe3
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-20 11:01:42 +0530

    Delete scripts directory

--- scripts/application_start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Starting application..." >> /var/log/deployment.log
-
-# Start the Apache service (or your application service)
-systemctl start httpd
-
-echo "Application started successfully" >> /var/log/deployment.log
