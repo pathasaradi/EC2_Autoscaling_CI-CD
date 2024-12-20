8dfa733a6e632b9d68773618d1e580b791c948f6
Challa Parthasaradi <117152440+pathasaradi@users.noreply.github.com>
2024-12-20 08:20:56 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# Validate that NGINX is running
-if systemctl is-active --quiet nginx; then
-    echo "NGINX is running."
-    exit 0
+echo "Validating service..." >> /var/log/deployment.log
+
+# Example validation by checking if the Apache service is running
+if systemctl is-active --quiet httpd; then
+  echo "Apache is running" >> /var/log/deployment.log
+  exit 0  # Return success
 else
-    echo "NGINX is not running."
-    exit 1
+  echo "Apache failed to start" >> /var/log/deployment.log
+  exit 1  # Return failure
 fi
